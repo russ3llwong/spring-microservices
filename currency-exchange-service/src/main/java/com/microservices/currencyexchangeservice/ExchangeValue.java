b5ad50e22550e10f340f3c81a426ab59adc09fa6
russ3llwong <russellwong97@hotmail.com>
2020-04-13 22:54:43 -0700

    Added currency-exchange-service

---
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,50 @@
+package com.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+public class ExchangeValue {
+	
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionMultiple;
+	private int port;
+
+	// JPA first creates a bean using the no-argument constructor and then calls the setters to set the values.
+	public ExchangeValue() {
+		
+	}
+	
+	public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+}
