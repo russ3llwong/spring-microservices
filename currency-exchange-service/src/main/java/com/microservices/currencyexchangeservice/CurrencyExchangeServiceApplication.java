445d017b4ef7dde182e3d93a6f202660fb64b7fa
russ3llwong <russellwong97@hotmail.com>
2020-04-14 19:38:12 -0700

    Added spring-cloud-sleuth and Sample to trace all requests for Gateway, CC and CE

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -3,6 +3,9 @@ package com.microservices.currencyexchangeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,12 @@ public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		// to trace all requests
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
--- zuul-api-gateway-server/src/main/java/com/microservices/zuulapigatewayserver/ZuulApiGatewayServerApplication.java
+++ zuul-api-gateway-server/src/main/java/com/microservices/zuulapigatewayserver/ZuulApiGatewayServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -14,4 +17,9 @@ public class ZuulApiGatewayServerApplication {
 		SpringApplication.run(ZuulApiGatewayServerApplication.class, args);
 	}
 
+	@Bean
+	public Sampler defaultSampler() {
+		// to trace all requests
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
