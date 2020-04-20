f0d9bd11102626cae280d12dbe2b840324f27284
russ3llwong <russellwong97@hotmail.com>
2020-04-19 19:08:24 -0700

    Handled invalid uri exception

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/exception/CustomizedExceptionHandler.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/exception/CustomizedExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.microservices.currencyexchangeservice.exception;
 
 import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -29,5 +33,17 @@ public class CustomizedExceptionHandler extends ResponseEntityExceptionHandler {
 		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 
+	@Override
+	protected ResponseEntity<Object> handleNoHandlerFoundException(
+			NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		String message = "Invalid URI. The only acceptable URI is \"/currency-exchange/from/{from}/to/{to}\", "
+				+ "where {from} and {to} are currencies.";
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), message,
+				request.getDescription(false));
+		
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
 	
 }
